de426c6588c3d5fdf1933f1a6b3f6a2c7644cba6
iamvedantp <vedantp183@gmail.com>
2024-07-25 18:10:24 +0530

    mario frame added mario jumping properly

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,7 @@ class MarioAnimationDemoState extends State<MarioAnimationDemo>
 
   late Animation<double> marioX;
   late Animation<double> marioY;
+  late Animation<int> marioFrame;
 
   @override
   void initState() {
@@ -32,7 +33,7 @@ class MarioAnimationDemoState extends State<MarioAnimationDemo>
 
     animationController = AnimationController(
       vsync: this,
-      duration: const Duration(seconds: 8),
+      duration: const Duration(seconds: 4), // Increase speed
     );
 
     marioX = TweenSequence<double>([
@@ -63,6 +64,22 @@ class MarioAnimationDemoState extends State<MarioAnimationDemo>
     super.dispose();
   }
 
+  int getCurrentFrame(double xValue, double yValue) {
+    if (xValue < 0.5) {
+      // Walking before the jump
+      return xValue % 0.1 < 0.05 ? 3 : 4;
+    } else if (xValue > 0.5) {
+      // Walking after the jump
+      return xValue % 0.1 < 0.05 ? 3 : 4;
+    } else if (yValue < 0.0) {
+      // Jumping
+      return 5;
+    } else {
+      // Stopping in the center
+      return 1;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -70,6 +87,7 @@ class MarioAnimationDemoState extends State<MarioAnimationDemo>
       body: AnimatedBuilder(
         animation: animationController,
         builder: (context, child) {
+          int currentFrame = getCurrentFrame(marioX.value, marioY.value);
           return Align(
             alignment: Alignment.centerLeft,
             child: Transform.translate(
@@ -77,8 +95,9 @@ class MarioAnimationDemoState extends State<MarioAnimationDemo>
                 marioX.value * MediaQuery.of(context).size.width,
                 marioY.value * 100,
               ),
-              child: const Image(
-                image: AssetImage("lib/assets/mario_1.png"),
+              child: Image.asset(
+                "lib/assets/mario_$currentFrame.png",
+                gaplessPlayback: true,
                 width: 50,
                 height: 50,
               ),
